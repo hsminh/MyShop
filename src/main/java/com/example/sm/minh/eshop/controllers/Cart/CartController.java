ed9115d59a62d2c1c0b2bfeb3b71ea9b4755ed49
sy Minh <hosyminh11820004@gmail.com>
2024-04-12 15:42:40 +0700

    c

--- src/main/java/com/example/sm/minh/eshop/controllers/Cart/CartController.java
+++ src/main/java/com/example/sm/minh/eshop/controllers/Cart/CartController.java
@@ -33,7 +33,7 @@
         public String viewCart(@RequestParam("productId") Integer productId, Model model, RedirectAttributes redirectAttributes) {
             try {
                 asdgkjerogjedorjgh
-                Product cartProduct = this.cartService.getProductById(productId);
+                Productb cartProduct = this.cartService.getProductById(productId);
                 model.addAttribute("pageTitle", "Cart ID |" + productId);
                 model.addAttribute("cartProduct", cartProduct);
                 return "cart/cart";
